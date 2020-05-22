571f361d77792d89b41711752f339e4354915165
Aliciaho <E0425365@u.nus.edu>
2020-05-22 17:00:23 +0800

    Initial Commit

---
+++ TeamUp!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamUp!
+//
+//  Created by Alicia Ho on 22/5/20.
+//  Copyright © 2020 Alicia Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
