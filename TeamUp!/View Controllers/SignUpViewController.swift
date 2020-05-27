2072a0f382b7226979fc3cd253746fb859744558
gua-guargia <60136318+gua-guargia@users.noreply.github.com>
2020-05-27 20:33:17 +0800

    database implemented

--- TeamUp!/View Controllers/SignUpViewController.swift
+++ TeamUp!/View Controllers/SignUpViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SignUpViewController: UIViewController {
 
@@ -63,9 +65,59 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    //check the text field and validate that the data is correct. If everything is correct, this method returns nil. Otherwise, it returns the error message
+    func validateFields() -> String? {
+        
+        //check that all fields are filled in
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        //check if a password is secured (regular expression of password)
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            //password isn't secure enough
+            return "Please make sure your password is at least 8 characters, contains a special character and number."
+        }
+        
+        return nil
+    }
 
     @IBAction func signUpTapped(_ sender: Any) {
+        //validate the field
+        let error = validateFields()
+        
+        if error != nil {
+            //there is something wrong with the fields, show error messge
+            showError(error!)
+        }
+        else {
+            //create the user
+            Auth.auth().createUser(withEmail: "", password: "") { (result, err) in
+                //check for errors
+                if err != nil {
+                    // there was an error creating the user
+                    self.showError("Error creating user")
+                }
+                else {
+                    // user was created successufully, now store the first name and last name
+                    let db = Firestore.firestore()
+                    db.collection("users")
+                }
+                
+            }
+            
+            //transition to the home screen
+        }
+        
     }
     
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
     
 }
