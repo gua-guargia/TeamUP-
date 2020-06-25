5e5c685ce5a4e25532ec413683682695ef7901e3
Aliciaho <E0425365@u.nus.edu>
2020-06-25 15:50:01 +0800

    combine done

--- TeamUp!/View Controllers/KolodaViewController.swift
+++ TeamUp!/View Controllers/KolodaViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 import Koloda
 import pop
 
+private let numberOfCards: Int = 5
+private let frameAnimationSpringBounciness: CGFloat = 9
+private let frameAnimationSpringSpeed: CGFloat = 16
+private let kolodaCountOfVisibleCards = 2
+private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
+
 class KolodaViewController: UIViewController {
 
   
@@ -19,14 +25,25 @@ class KolodaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        kolodaView.layer.cornerRadius = 20
-        kolodaView.clipsToBounds = true
+       // kolodaView.layer.cornerRadius = 20
+       // kolodaView.clipsToBounds = true
         
         // Do any additional setup after loading the view.
-        kolodaView.dataSource = self
+      //  kolodaView.dataSource = self
+       // kolodaView.delegate = self
+        
+        kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
+        kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
         kolodaView.delegate = self
+        kolodaView.dataSource = self
+        kolodaView.animator = KolodaViewAnimator(koloda: kolodaView)
+        
+        self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
         
         
+    }
+    @IBAction func revertButton(_ sender: Any) {
+        kolodaView?.revertAction()
     }
 }
 
@@ -41,13 +58,32 @@ class KolodaViewController: UIViewController {
     */
     extension KolodaViewController: KolodaViewDelegate {
         func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
-            koloda.reloadData()
+            kolodaView.resetCurrentCardIndex()
         }
 
         func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
            let alert = UIAlertController(title: "Congratulation!", message: "Now you're \(images[index])", preferredStyle: .alert)
            alert.addAction(UIAlertAction(title: "OK", style: .default))
            self.present(alert, animated: true)
+        }
+        
+        func kolodaShouldApplyAppearAnimation(_ koloda: KolodaView) -> Bool {
+            return true
+        }
+        
+        func kolodaShouldMoveBackgroundCard(_ koloda: KolodaView) -> Bool {
+            return false
+        }
+        
+        func kolodaShouldTransparentizeNextCard(_ koloda: KolodaView) -> Bool {
+            return true
+        }
+        
+        func koloda(kolodaBackgroundCardAnimation koloda: KolodaView) -> POPPropertyAnimation? {
+            let animation = POPSpringAnimation(propertyNamed: kPOPViewFrame)
+            animation?.springBounciness = frameAnimationSpringBounciness
+            animation?.springSpeed = frameAnimationSpringSpeed
+            return animation
         }
     }
 
